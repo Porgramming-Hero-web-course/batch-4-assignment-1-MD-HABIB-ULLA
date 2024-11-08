7b93fd12b85798fc28166a9a241a2b2e0cd1a7d4
habib ulla <habibulla1278@gmail.com>
2024-11-08 09:46:33 +0600

    problem-5 solved

---
+++ problem-5.ts
@@ -0,0 +1,21 @@
+interface Profile {
+  name: string;
+  age: number;
+  email: string;
+}
+
+const updateProfile = (
+  profile: Profile,
+  updates: Partial<Profile>
+): Profile => {
+  return { ...profile, ...updates };
+};
+
+const myProfile: Profile = {
+  name: "Alice",
+  age: 25,
+  email: "alice@example.com",
+};
+const updatedProfile = updateProfile(myProfile, { age: 26 });
+
+console.log(updatedProfile); 
